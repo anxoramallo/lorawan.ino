39f409b7e2b993dacf4bb2d77f0d9ca261a2950d
anxoramallo <anxoramallo388@gmail.com>
2024-05-16 21:18:23 +0200

    Create LORAreceptor.ino

---
+++ LORAreceptor.ino
@@ -0,0 +1,24 @@
+#define ledpin 2
+String incomingString;
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledpin, OUTPUT);
+  Serial.println("AT+ADDRESS=155");  //0~65535
+  delay(500);
+  Serial.println("AT+NETWORKID=3");  //=3~15,18(default18)
+  delay(500);
+  Serial.println("AT+BAND=928000000");  //915M a 928M --> Caso Colombia
+  delay(1500);
+}
+void loop() {
+  if (Serial.available()) {
+    incomingString = Serial.readString();
+    if (incomingString.indexOf("PREND") > 0) {
+      digitalWrite(ledpin, HIGH);
+      delay(500);
+      digitalWrite(ledpin, LOW);
+    } else {
+      digitalWrite(ledpin, LOW);
+    }
+  }
+}
